1176c36d7303be704f05dbea9ff1dc21b6b038b9
juanosarg <31105173+juanosarg@users.noreply.github.com>
2022-01-15 12:25:40 +0100

    v0.1

---
+++ Source/VanillaIdeologyExpanded Dryads/VanillaIdeologyExpanded Dryads/Comps/CompNotifyDestruction.cs
@@ -0,0 +1,43 @@
+﻿
+using Verse;
+
+namespace VanillaIdeologyExpanded_Dryads
+{
+    class CompNotifyDestruction : ThingComp
+    {
+
+
+        public CompProperties_NotifyDestruction Props
+        {
+            get
+            {
+                return (CompProperties_NotifyDestruction)this.props;
+            }
+        }
+
+      
+
+        public override void PostDeSpawn(Map map)
+        {
+            NotifyDestructionToComps();
+        }
+
+        public override void PostDestroy(DestroyMode mode, Map previousMap)
+        {
+            NotifyDestructionToComps();
+        }
+
+        public void NotifyDestructionToComps()
+        {
+            foreach (Pawn p in this.parent.Map.mapPawns.SpawnedColonyAnimals)
+            {
+                AnimalBehaviours.CompBuildPeriodically comp = p.TryGetComp<AnimalBehaviours.CompBuildPeriodically>();
+                if (comp != null)
+                {
+                    comp.NotifyBuildingDestroyed(this.parent);
+                }
+            }
+
+        }
+    }
+}
---
+++ Source/VanillaIdeologyExpanded Dryads/VanillaIdeologyExpanded Dryads/Comps/Properties/CompProperties_NotifyDestruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using RimWorld;
+using Verse;
+using UnityEngine;
+
+using Verse.AI;
+
+namespace VanillaIdeologyExpanded_Dryads
+{
+    public class CompProperties_NotifyDestruction : CompProperties
+    {
+       
+        public CompProperties_NotifyDestruction()
+        {
+            this.compClass = typeof(CompNotifyDestruction);
+        }
+    }
+}
